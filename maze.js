a68915f7f9088083373ed1b5ace77e38cee3be9d
thevirtuoso1973 <chris.tomy@hotmail.com>
2019-08-02 20:59:57 +0100

    Add comment

--- maze.js
+++ maze.js
@@ -12,6 +12,7 @@
 walls are present.
 */
 
+//Prevents arrow key scrolling:
 window.addEventListener("keydown", function(e) {
     // space and arrow keys
     if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
