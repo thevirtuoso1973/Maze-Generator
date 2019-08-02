11b4d5f6d46f1ec2989b029323925f33516a48cc
thevirtuoso1973 <chris.tomy@hotmail.com>
2019-08-02 20:59:25 +0100

    Prevents arrow key scrolling

--- maze.js
+++ maze.js
@@ -12,6 +12,13 @@
 walls are present.
 */
 
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 //LOGIC (maze):
 let visited = {} //object that holds visited cells, acts as global var
 function generateMaze() {
